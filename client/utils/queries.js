3338dc12e55350a0faab66dad3bfa858bfc7ff73
brianlu18 <brianlu18@g.ucla.edu>
2023-08-02 23:44:13 -0500

    finished queries and auth.js

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,2 +1,20 @@
 import { gql } from "@apollo/client";
 
+export const QUERY_USER = gql`
+  query  {
+    user {
+      _id
+      username
+      email
+      password
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
