d232962b0bf9980875735322756488cad636c15b
brianlu18 <brianlu18@g.ucla.edu>
2023-08-02 23:51:56 -0500

    added and finished mutations.js

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -1,3 +1,61 @@
 import { gql } from '@apollo/client';
 
-export const 
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!){
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookData: BookInput!) {
+    removeBook(bookData: $bookData){
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email:$email, password:$password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($email: String!, $password: String!, $username: String!) {
+    addUser(email:$email, password: $password, username: $username) {
+      token 
+      user {
+        _id
+        username
+      }
+    }
+  }
+`
